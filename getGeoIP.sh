0c2f27ac1c68caa45e6489fceade250ece289645
Landon Mayo <synackme@gmail.com>
2014-03-25 20:06:52 -0500

    used egrep to better control output

--- getGeoIP.sh
+++ getGeoIP.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl -s -A Moz geoiptool.com | html2text | egrep -A11 __________________________________________________________________
+curl -s -A Moz http://www.geoiptool.com | html2text | egrep -e 'Host Name' -e 'IP Address' -e 'Country' -e 'Country code' -e 'Region' -e 'City' -e 'Postal code' -e 'Calling code' -e 'Longitude' -e 'Latitude'
+#| egrep  "__________________________________________________________________"
 echo ""
